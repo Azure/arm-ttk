e277464f1fb6e9da5619bf0766655e93f6c1203c
James Brundage <james.brundage@start-automating.com>
2020-03-04 13:18:19 -0800

    Adding Check for RegEx Length Constraints

--- arm-ttk/testcases/CreateUIDefinition/Textboxes-Are-Well-Formed.test.ps1
+++ arm-ttk/testcases/CreateUIDefinition/Textboxes-Are-Well-Formed.test.ps1
@@ -9,6 +9,8 @@ $CreateUIDefinitionObject
 
 $allTextBoxes = $CreateUiDefinitionObject | Find-JsonContent -Key type -value Microsoft.Common.TextBox
 
+$LengthConstraint = [Regex]::new('(?<!\\)\{(?<Min>\d+),(?<Max>\d+)\}')
+
 foreach ($textbox in $allTextBoxes) { # Then we walk over each textbox.
     if (-not $textbox.constraints) { # If constraints was missing or blank,
         Write-Error "Textbox $($textbox.Name) is missing constraints" -TargetObject $textbox # error
@@ -22,6 +24,10 @@ foreach ($textbox in $allTextBoxes) { # Then we walk over each textbox.
         } catch {
             $err = $_ # if that fails, 
             Write-Error "Textbox $($textbox.Name) regex is invalid: $($err)" -TargetObject $textbox #error.
+        }
+        if (-not $LengthConstraint.IsMatch($textbox.constraints.regex)) { # See if it has a length constraint
+            # If not, error
+            Write-Error "TextBox $($textbox.Name) regex is missing a length constraint" -TargetObject $textbox 
         }
     }
     if (-not $textbox.constraints.validationMessage) { # If there's not a validation message
