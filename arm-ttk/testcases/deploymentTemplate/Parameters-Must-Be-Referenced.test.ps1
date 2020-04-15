5ae47ddbb76a09bb547ef0add632c97042957dac
James Brundage <james.brundage@start-automating.com>
2020-04-15 13:50:20 -0700

    Updating Test

--- arm-ttk/testcases/deploymentTemplate/Parameters-Must-Be-Referenced.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Parameters-Must-Be-Referenced.test.ps1
@@ -23,24 +23,16 @@ An expression could be: "[ concat ( variables ( 'test' ), ...)]"
 #>
 $exprStrOrQuote = [Regex]::new('(?<!\\)(?>"\s{0,}\[")', 'RightToLeft')
 foreach ($parameter in $TemplateObject.parameters.psobject.properties) {
-
-    if (!($parameter.name.startswith('__')) -and ($parameter.name -ne 'copy') ) {
-        $findVar = [Regex]::new("parameters\s{0,}\(\s{0,}'$($Parameter.Name)'\s{0,}\)")
-        $foundRefs = @($findVar.Matches($TemplateText))
-        if (-not $foundRefs) {
-            Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
-        } else {
-            foreach ($fr in $foundRefs) {
-                $foundQuote =$exprStrOrQuote.Match($TemplateText, $fr.Index)                
-                if ($foundQuote.Value -eq '"') {
-                    Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
-                }
+    $findVar = [Regex]::new("parameters\s{0,}\(\s{0,}'$($Parameter.Name)'\s{0,}\)")
+    $foundRefs = @($findVar.Matches($TemplateText))
+    if (-not $foundRefs) {
+        Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
+    } else {
+        foreach ($fr in $foundRefs) {
+            $foundQuote =$exprStrOrQuote.Match($TemplateText, $fr.Index)                
+            if ($foundQuote.Value -eq '"') {
+                Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
             }
         }
-
     }
 }
- 
-
-
-
