4d70e3111dac8a1e9a03cb83c4dae1f78072e665
James Brundage <james.brundage@start-automating.com>
2020-04-22 12:23:14 -0700

    Updating Documentation

--- arm-ttk/testcases/deploymentTemplate/Parameters-Property-Must-Exist.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Parameters-Property-Must-Exist.test.ps1
@@ -1,9 +1,17 @@
-﻿param(
-[Parameter(Mandatory=$true,Position=0)]
-[PSObject]
-$TemplateObject
+﻿<#
+.Synopsis
+    Ensures that the 'parameters' property exists in a template
+.Description
+    Ensures that an Azure Resource Manager template contains the 'parameters' property.
+#>
+param(
+    # The template object
+    [Parameter(Mandatory=$true,Position=0)]
+    [PSObject]
+    $TemplateObject
 )
+
 # TODO: do we still need this test?  it's implied by checking for other parameters (e.g. location)
 if (-not $TemplateObject.psobject.properties.item('parameters')) {
-    Write-Error -Message "Parameters property must exist in the template"
+    Write-Error -Message "Parameters property must exist in the template" -ErrorId 'Template.Missing.Parameters'
 } 
