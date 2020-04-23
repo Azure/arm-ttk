488db62de1e6ae2a36b79c9514d9fe84df1a5931
James Brundage <james.brundage@start-automating.com>
2020-04-22 22:30:43 -0700

    Switching to Regular Expression, as wildcards were missing hardcoded values

--- arm-ttk/testcases/deploymentTemplate/Secure-String-Parameters-Cannot-Have-Default.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Secure-String-Parameters-Cannot-Have-Default.test.ps1
@@ -11,6 +11,18 @@ param(
     $TemplateObject
 )
 
+$usedNewGuid = [Regex]::new(@'
+\[             # Starting bracket 
+\s{0,}         # ... optional whitespace
+newGuid        # the literal 'newGuid'
+\s{0,}         # ... optional whitepace
+\(             # open parenthesis
+\s{0,}         # optional whitespace
+\)             # close parenthesis
+\s{0,}         # optional whitespace
+\]             # Closing bracket
+'@, 'Multiline,IgnoreCase,IgnorePatternWhitespace')
+
 # Walk thru each parameter in the template object
 foreach ($parameterProp in $templateObject.parameters.psobject.properties) {
     $parameter = $parameterProp.Value
@@ -19,7 +31,8 @@ foreach ($parameterProp in $templateObject.parameters.psobject.properties) {
     # If the parameter is a secureString type and has a defaultValue:
     if ($parameter.Type -eq 'securestring' -and $parameter.defaultValue) { 
         # the defaultValue must be an empty string "" or must be an expression that contains use the newGuid() function
-        if ($parameter.defaultValue -ne "" -and $parameter.defaultValue -notlike '*[*newGuid*(*)*]*') {
+        if ($parameter.defaultValue -and 
+            $parameter.defaultValue -notmatch $usedNewGuid) {
             # Will return true when defaultvalue is not null or blank (blank values are OK). 
             Write-Error -Message "Parameter $name is a SecureString and must not have a default value unless it is an expression that contains the newGuid() function." `
                 -ErrorId SecureString.Must.Not.Have.Default -TargetObject $parameter
