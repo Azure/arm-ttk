62884ad66ba573eee5348753cba68c1c060c5260
James Brundage <james.brundage@start-automating.com>
2020-04-30 13:45:15 -0700

    Updating Test to fix Case-Sensitivity Issue.  Making Regex More Clear.  Adding Docs.

--- arm-ttk/testcases/deploymentTemplate/Parameters-Must-Be-Referenced.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Parameters-Must-Be-Referenced.test.ps1
@@ -1,36 +1,49 @@
-﻿param(
+﻿<#
+.Synopsis
+    Ensures that parameters are referenced
+.Description
+    Ensures that all Azure Resource Manager Template
+#>
+param(
+    # The Template Object
     [Parameter(Mandatory = $true, Position = 0)]
     [PSObject]
     $TemplateObject,
 
+    # The Template JSON Text
     [Parameter(Mandatory = $true, Position = 0)]
     [PSObject]
     $TemplateText
 )
 
-<# REGEX
-- 
-- parameters
-- whitespace
-- ('
-- whitespace
-- <ParameterName>
-- whitespace
-- ')
-
-An expression could be: "[ concat ( variables ( 'test' ), ...)]"
-
-#>
 $exprStrOrQuote = [Regex]::new('(?<!\\)(?>"\s{0,}\[")', 'RightToLeft')
 foreach ($parameter in $TemplateObject.parameters.psobject.properties) {
-    $findVar = [Regex]::new("parameters\s{0,}\(\s{0,}'$($Parameter.Name)'\s{0,}\)")
-    $foundRefs = @($findVar.Matches($TemplateText))
-    if (-not $foundRefs) {
+    # If the parameter name starts with two underscores,
+    if ($parameter.Name -like '__*') { continue } # skip it.
+
+
+    # Create a Regex to find the var
+    $findVar = [Regex]::new(@"
+parameters           # the parameters keyword
+\s{0,}               # optional whitespace
+\(                   # opening parenthesis
+\s{0,}               # more optional whitespace
+(?:'|\\u0027)        # either a single quote or an escaped single quote
+$($Parameter.Name)   # the parameter name
+(?:'|\\u0027)        # either a single quote or an escaped single quote
+\s{0,}               # more optional whitespace
+\)                   # closing parenthesis
+"@,
+    # The Regex needs to be case-insensitive
+'Multiline,IgnoreCase,IgnorePatternWhitespace'
+)
+    $foundRefs = $findVar.Matches($TemplateText) # See if we found the variable
+    if (-not $foundRefs) { # If we didn't, error
         Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
     } else {
-        foreach ($fr in $foundRefs) {
-            $foundQuote =$exprStrOrQuote.Match($TemplateText, $fr.Index)                
-            if ($foundQuote.Value -eq '"') {
+        foreach ($fr in $foundRefs) { # Walk thru each reference
+            $foundQuote =$exprStrOrQuote.Match($TemplateText, $fr.Index) # make sure we hit a [ before a quote
+            if ($foundQuote.Value -eq '"') { # if we don't, error
                 Write-Error -Message "Unreferenced parameter: $($Parameter.Name)" -ErrorId Parameters.Must.Be.Referenced -TargetObject $parameter
             }
         }
