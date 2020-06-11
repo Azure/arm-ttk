c95c4c3013c9ea3524bc24c781534abb50d16ae5
James Brundage <james.brundage@start-automating.com>
2020-06-11 12:22:20 -0700

    Adding test for Deployment Resources DebugSetting (4144)

---
+++ arm-ttk/testcases/deploymentTemplate/Deployment-Resources-Must-Not-Be-Debug.test.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.Synopsis
+    Ensures Deployment Resources Must not use DebugSetting
+.Description
+    Ensures resources of type Microsoft.Resources/deployments do not have 
+    their DebugSetting property set (to a value other than 'None').
+#>
+param(
+# The Template Object
+[Parameter(Mandatory)]
+[PSObject]
+$TemplateObject
+)
+
+$deploymentResources = $TemplateObject.resources | 
+    Find-JsonContent -Key type -Value 'Microsoft.Resources/deployments'
+
+
+foreach ($dr in $deploymentResources) {
+    if ($dr.DebugSetting -and $dr.DebugSetting -ne 'None') {
+        Write-Error "Deployment Resources must have no DebugSettings property, or must set it to 'None'" -TargetObject $dr -ErrorId "Deployment.Resource.Has.DebugSetting"
+    }
+}  
