27476dfd3d23d14a7c993e68f42c6448dcf0e4e1
James Brundage <james.brundage@start-automating.com>
2020-06-18 02:26:59 -0700

    Adding Validation for Expressions in Parameter/Variable Names

---
+++ arm-ttk/testcases/deploymentTemplate/Expressions-May-Not-Be-Used-As-Parameter-Or-Variable-Names.test.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.Synopsis
+    Ensures Expressions may not be used to name parameters/variables
+.Description
+    Ensures that Expressions may not be used in the name of a parameter or variable.
+#>
+param(
+[Parameter(Mandatory)]
+[PSObject]
+$TemplateObject
+)
+
+
+$expressionStart = '^\s{0,}\['
+$expressionEnd = '\]\s{0,}$'
+
+$variableNames = $TemplateObject.variables.psobject.properties | Select-Object -ExpandProperty Name
+foreach ($vn in $variableNames) {
+    if ($vn -match $expressionStart -and $vn -match $expressionEnd) {
+        Write-Error "Variable name is an expression: $vn" -TargetObject $vn -ErrorId 'Variable.Name.Is.Expression' 
+    }
+}
+ 
+$parameterNames = $TemplateObject.parameters.psobject.properties | Select-Object -ExpandProperty Name
+foreach ($pn in $parameterNames) {
+    if ($pn -match $expressionStart -and $pn -match $expressionEnd) {
+        Write-Error "Parameter name is an expression: $pn" -TargetObject $pn -ErrorId 'Parameter.Name.Is.Expression' 
+    }
+}
