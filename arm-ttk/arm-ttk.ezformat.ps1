78889f4f4dd1beef0320e28b7f931e2ba8f6a64b
James Brundage <james.brundage@start-automating.com>
2020-06-18 11:50:14 -0700

    Updating .ezformat.ps1 (requires latest EZOut)

--- arm-ttk/arm-ttk.ezformat.ps1
+++ arm-ttk/arm-ttk.ezformat.ps1
@@ -2,13 +2,13 @@
 #                 https://github.com/StartAutomating/EZOut
 #              OR Install-Module EZOut   
 $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
-$myName = $MyInvocation.MyCommand.Name -replace '\.ezformat\.ps1', ''
+$myName = 'arm-ttk'
 
 
 Write-FormatView -Action {
-    $h = Get-History -Count 1
+    $h = $MyInvocation.HistoryId
     $testOut = $_
-    if ($global:_LastHistoryId -ne $h.id) {
+    @(if ($global:_LastHistoryId -ne $h.id) {
         # New scoppe
         $global:_LastGroup = ''
         $global:_LastFile = ''
@@ -17,14 +17,26 @@ Write-FormatView -Action {
     
 
     if ($global:_LastFile -ne $testOut.File.FullPath) {
-        Write-Host -ForegroundColor Magenta "Validating $($testOut.File.FullPath | Split-Path | split-Path -Leaf)\$($testOut.File.Name)" 
-
-        $global:_LastFile = $testOut.File.FullPath    
+        $msg = "Validating $($testOut.File.FullPath | Split-Path | split-Path -Leaf)\$($testOut.File.Name)"
+        if ($host.UI.SupportsVirtualTerminal) {
+            . $SetOutputStyle -ForegroundColor Magenta        
+            $msg
+            . $clearOutputStyle
+        } else {
+            Write-Host -ForegroundColor Magenta -NoNewline $msg
+        }
+        $global:_LastFile = $testOut.File.FullPath
     }
 
     if ($global:_LastGroup -ne $testOut.Group) {
-        $global:_LastGroup = $testOut.Group
-        Write-Host -ForegroundColor Magenta "  $($testOut.Group)" 
+        if ($host.UI.SupportsVirtualTerminal) {
+            . $SetOutputStyle -ForegroundColor Magenta
+            $global:_LastGroup = $testOut.Group
+            "  $($testOut.Group)" 
+            . $clearoutputStyle
+        } else {
+            Write-Host "  $($testOut.Group)" -ForegroundColor Magenta
+        }
     }
     $errorCount = $testOut.Errors.Count
     $warningCount = $testOut.Warnings.Count
@@ -53,33 +65,70 @@ Write-FormatView -Action {
     }
     
     $statusLine = "    [$statusChar] $($testOut.Name) ($([Math]::Round($testOut.Timespan.TotalMilliseconds)) ms)"
-    Write-Host $statusLine -ForegroundColor $foregroundColor -NoNewline
+    if ($host.UI.SupportsVirtualTerminal) {
+        . $setOutputStyle -ForegroundColor $foregroundColor
+        $statusLine
+        . $clearOutputStyle
+    } else {
+        Write-Host $statusLine -NoNewline -ForegroundColor $foregroundColor
+    }
 
     $azoErrorStatus = if ($ENV:Agent_ID) { "##vso[task.logissue type=error;]"} else { '' }
-    
+    $azoWarnStatus  = if ($ENV:Agent_ID) { "##vso[task.logissue type=warning;]"} else { '' }
     $indent = 8
     if ($testOut.AllOutput) {
-        Write-Host " " # end of line
-         
+        if (-not $host.UI.SupportsVirtualTerminal) {
+            Write-Host ' '
+        } else {
+            [Environment]::NewLine
+        } 
         foreach ($line in $testOut.AllOutput) {
             if ($line -is [Management.Automation.ErrorRecord] -or $line -is [Exception]) {
-                Write-Host "$azoErrorStatus$(' ' * $indent)$line" -foregroundColor Red
+                $msg = "$azoErrorStatus$(' ' * $indent)$line"
+                if ($line.TargetObject -is [Text.RegularExpressions.Match]) {
+                    $msg += (" Index:" + $line.TargetObject.Index)
+                }
+
+
+                if ($host.UI.SupportsVirtualTerminal) {
+                    . $setOutputStyle -ForegroundColor Red
+                    $msg
+                    . $ClearOutputStyle
+                } else {
+                    Write-Host -ForegroundColor Red $msg
+                }
             }
             elseif ($line -is [Management.Automation.WarningRecord]) {
-                Write-Host "$azoWarnStatus$(' ' * $indent)$line" -foregroundColor Yellow
+                $msg = "$azoWarnStatus$(' ' * $indent)$line"
+                if ($host.UI.SupportsVirtualTerminal) {
+                    . $setOutputStyle -ForegroundColor Yellow
+                    $msg
+                    . $clearOutputStyle
+                } else {
+                    Write-Host -ForegroundColor Yellow $msg 
+                }
             }
             elseif ($line -is [string]) {
-                Write-Host "$(' ' * $indent)$line"
-            } 
+                $msg = "$(' ' * $indent)$line"
+                if ($host.UI.SupportsVirtualTerminal) {
+                    $msg
+                } else {
+                    Write-Host $msg
+                }
+            }
             else {
                 $line | 
                     Out-String -Width ($Host.UI.RawUI.BufferSize.Width - $indent) |
                     & { process {
-                        Write-Host "$(' ' * $indent)$_"
+                        if ($host.UI.SupportsVirtualTerminal) {
+                            "$(' ' * $indent)$_"
+                        } else {
+                            Write-Host "$(' ' * $indent)$_"
+                        }
                     } } 
             }
         }
-    }    
+    }) -join ''
 } -TypeName 'Template.Validation.Test.Result' |
     Out-FormatData |
     Set-Content -Path (Join-Path $myRoot "$myName.format.ps1xml") -Encoding UTF8
