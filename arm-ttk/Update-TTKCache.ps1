d1fa1386d8548472d3ed7936cc1a0373f3a5cf55
James Brundage <james.brundage@start-automating.com>
2020-06-18 12:03:37 -0700

    Adding Update-TTKCache

---
+++ arm-ttk/Update-TTKCache.ps1
@@ -0,0 +1,20 @@
+﻿function Update-TTKCache
+{
+    <#
+    .Synopsis
+        Updates the TTK Cache
+    .Description
+        Updates the /cache/ information in the Azure Resource Manager Template Toolkit
+    .Example
+        Update-TTKCache
+    #>
+    param()
+
+    $MyInvocation.MyCommand.ScriptBlock.File | 
+        Split-Path | 
+        Join-Path -ChildPath cache |
+        Get-ChildItem -Filter *.init.cache.ps1 |
+        ForEach-Object {
+            & $_.Fullname
+        }
+}
