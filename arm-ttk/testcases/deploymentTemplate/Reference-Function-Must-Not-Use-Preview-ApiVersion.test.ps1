531294f64fea1e272acd68a0b02811acd27f5e62
James Brundage <james.brundage@start-automating.com>
2020-06-18 04:12:03 -0700

    Adding Validation for Reference Preview ApiVersions (4162)

---
+++ arm-ttk/testcases/deploymentTemplate/Reference-Function-Must-Not-Use-Preview-ApiVersion.test.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.Synopsis
+    Ensures that reference functions do not use preview ApiVersions.
+.Description
+    Ensures that the uses of the reference() functions within an 
+    Azure Resource Manager template do not refer to a -preview.
+#>
+param(
+[Parameter(Mandatory)]
+[string]
+$TemplateText
+)
+
+$matchSegments = 
+    'reference',
+    '\(',
+        "'(?<ResourceName>[^']+)'",
+        ',',
+        "'(?<ApiVersion>[^']+)'"
+
+$referenceWithApiVersion = [Regex]::new(
+    $matchSegments -join ([Environment]::NewLine + '\s?' + [Environment]::NewLine), 
+    'IgnoreCase,IgnorePatternWhitespace')
+
+foreach ($match in $referenceWithApiVersion.Matches($TemplateText)) {
+    if ($match.Groups['ApiVersion'] -like '*-preview') {
+        Write-Error "Referencing a -preview apiversion for $($match.Groups['ResourceName'])" -TargetObject $match
+    }
+}
