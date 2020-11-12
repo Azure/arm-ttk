e4c5473dba8652c24c8faa85fbbbe509faa93c1f
James Brundage <james.brundage@start-automating.com>
2020-11-12 12:31:55 -0800

    Adding Regex ${?<ARM_Variable>}

---
+++ arm-ttk/Regex/ARM/Variable.regex.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.Synopsis
+    Matches an ARM variable
+.Description
+    Matches an Azure Resource Manager template variable.
+#>
+param(
+# The name of the variable.  By default, matches any variable.
+[string]
+$Variable = '.+?'
+)
+
+@"
+variables                       # the variables keyword
+\s?                             # optional whitespace
+\(                              # opening parenthesis
+\s{0,}                          # more optional whitespace
+'                               # a single quote
+(?<VariableName>                # followed by the variable name
+$($Variable -replace '\s','\s')    
+)
+'                               # a single quote
+\s{0,}                          # more optional whitespace
+\)                              # closing parenthesis
+"@
+
+
