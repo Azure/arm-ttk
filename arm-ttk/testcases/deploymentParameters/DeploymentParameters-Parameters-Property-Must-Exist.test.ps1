982caebc174db61cf0cfe67140e316aa585260f1
Robert Angers <robert.angers@gmail.com>
2020-11-22 17:21:53 -0500

    Added tests for DeploymentParameters
    DeploymentParameters-ContentVersion-Must-Exist
    DeploymentParameters-Parameters-Property-Must-Exist
    DeploymentParameters-Parameters-Property-Must-Have-Value

---
+++ arm-ttk/testcases/deploymentParameters/DeploymentParameters-Parameters-Property-Must-Exist.test.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.Synopsis
+    Ensures that the 'parameters' property exists in a template
+.Description
+    Ensures that an Azure Resource Manager template contains the 'parameters' property.
+#>
+param(
+    # The template object
+    [Parameter(Mandatory=$true,Position=0)]
+    [PSObject]
+    $TemplateObject
+)
+
+if (-not $TemplateObject.psobject.properties.item('parameters')) {
+    Write-Error -ErrorId Parameters.Missing.Parameters -Message "Parameters property must exist in the parameters file" 
+} 
