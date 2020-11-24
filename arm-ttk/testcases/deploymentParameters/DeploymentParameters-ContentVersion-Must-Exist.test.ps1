45a6d727e94a04b547af059e3179d9ac1f11c275
Robert Angers <robert.angers@gmail.com>
2020-11-24 16:36:01 -0500

    added support for keyvault parameters
    casing camelcase

--- arm-ttk/testcases/deploymentParameters/DeploymentParameters-ContentVersion-Must-Exist.test.ps1
+++ arm-ttk/testcases/deploymentParameters/DeploymentParameters-ContentVersion-Must-Exist.test.ps1
@@ -6,24 +6,24 @@
     And that is formatted like a proper version, ie 1.0.0.0
 #>
 param(
-    # The template object
-    [Parameter(Mandatory=$true,Position=0)]
-    [PSObject]
-    $TemplateObject
+  # The template object
+  [Parameter(Mandatory=$true,Position=0)]
+  [PSObject]
+  $TemplateObject
 )
 
 if (-not $TemplateObject.psobject.properties.item('contentVersion')) {
     Write-Error -ErrorId Parameters.Missing.ContentVersion -Message "contentVersion property must exist in the template"
-    return;
+    continue
 } 
 
 if ($TemplateObject.contentVersion -isnot [string]) {
     Write-Error -ErrorId ContentVersion.Not.String -Message "contentVersion must be string" -TargetObject $TemplateObject.contentVersion 
-    return;
+    continue
 } 
 
-if ($TemplateObject.contentVersion -notmatch '\d{1,3}\.\d{1,3}\.\d{1,5}\.\d{1,5}') {
-    Write-Error -ErrorId ContentVersion.Bad.Format -TargetObject $TemplateObject.contentVersion  -Message "contentVersion must be a version string 
+if ($TemplateObject.contentVersion -inotmatch '\d{1,3}\.\d{1,3}\.\d{1,5}\.\d{1,5}') {
+    Write-Warning -Message "Recommended that the 'contentVersion' should be a version string 
     Like:  1.0.0.0  -> but found: $($TemplateObject.contentVersion)"
-    return;
+    continue
 } 
