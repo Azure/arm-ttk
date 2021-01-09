476bd659d70d226e034a8cbacb5f70edb13bc623
Severin Schols <severin@schols.de>
2021-01-10 00:26:53 +0100

    cleaner way, with proper checks beforehand

--- arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
@@ -11,28 +11,32 @@ $TemplateObject
 )
 
 # Find all references to an adminUserName
-# Filterting the complete $TemplateObject directly fails with "The script failed due to call depth overflow." errors
+# Filtering the complete $TemplateObject directly fails with "The script failed due to call depth overflow." errors
 
-$adminUserNameRefsResources = $TemplateObject.resources |
-    Find-JsonContent -Key adminUsername  -Value * -Like |
-    Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
-$adminUserNameRefsVariables = $TemplateObject.variables |
-    Find-JsonContent -Key adminUsername  -Value * -Like |
-    Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
+if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
+    $adminUserNameRefsResources = $TemplateObject.resources |
+        Find-JsonContent -Key adminUsername  -Value * -Like |
+        Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
 
-
-foreach ($ref in $adminUserNameRefsResources) { # Walk over each one
-    $trimmedUserName = "$($ref.adminUserName)".Trim()
-    if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
-        Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
-        continue # and move onto the next
+    foreach ($ref in $adminUserNameRefsResources) { # Walk over each one
+        $trimmedUserName = "$($ref.adminUserName)".Trim()
+        if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
+            Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
+            continue # and move onto the next
+        }
     }
 }
 
-foreach ($ref in $adminUserNameRefsVariables) { # Walk over each one
-    $trimmedUserName = "$($ref.adminUserName)".Trim()
-    if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
-        Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
-        continue # and move onto the next
+if ("variables" -in $TemplateObject.PSobject.Properties.Name) {
+    $adminUserNameRefsVariables = $TemplateObject.variables |
+        Find-JsonContent -Key adminUsername  -Value * -Like |
+        Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
+
+    foreach ($ref in $adminUserNameRefsVariables) { # Walk over each one
+        $trimmedUserName = "$($ref.adminUserName)".Trim()
+        if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
+            Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
+            continue # and move onto the next
+        }
     }
 }
