908fe05819177d7bc76620bd8d04940770125d97
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2021-01-21 17:52:20 -0600

    add empty test template test case, fixes

--- arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
@@ -5,9 +5,9 @@
     Ensures that all properties within a template named adminUsername are expressions, not literal strings
 #>
 param(
-[Parameter(Mandatory=$true)]
-[PSObject]
-$TemplateObject
+    [Parameter(Mandatory = $true)]
+    [PSObject]
+    $TemplateObject
 )
 
 # Find all references to an adminUserName
@@ -15,12 +15,14 @@ $TemplateObject
 
 if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
     $adminUserNameRefsResources = $TemplateObject.resources |
-        Find-JsonContent -Key adminUsername  -Value * -Like |
-        Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
+    Find-JsonContent -Key adminUsername  -Value * -Like |
+    Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
 
-    foreach ($ref in $adminUserNameRefsResources) { # Walk over each one
+    foreach ($ref in $adminUserNameRefsResources) {
+        # Walk over each one
         $trimmedUserName = "$($ref.adminUserName)".Trim()
-        if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
+        if ($trimmedUserName -notmatch '\[[^\]]+\]') {
+            # If they aren't expressions
             Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
             continue # and move onto the next
         }
@@ -29,26 +31,31 @@ if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
 
 if ("variables" -in $TemplateObject.PSobject.Properties.Name) {
     $adminUserNameRefsVariables = $TemplateObject.variables |
-        Find-JsonContent -Key adminUsername  -Value * -Like |
-        Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
+    Find-JsonContent -Key adminUsername  -Value * -Like |
+    Where-Object { -not $_.ParentObject[0].'$schema' } # unless they're on a top-level property.
 
-    foreach ($ref in $adminUserNameRefsVariables) { # Walk over each one
+    foreach ($ref in $adminUserNameRefsVariables) {
+        # Walk over each one
         $trimmedUserName = "$($ref.adminUserName)".Trim()
-        if ($trimmedUserName -notmatch '\[[^\]]+\]') { # If they aren't expressions
+        if ($trimmedUserName -notmatch '\[[^\]]+\]') {
+            # If they aren't expressions
             Write-Error -TargetObject $ref -Message "AdminUsername is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
             continue # and move onto the next
         }
     }
 
-    $UserNameHasVariable = $trimmedUserName | ?<ARM_Variable> -Extract
+    # TODO - irregular doesn't handle null gracefully so we need to test for it
+    if ($trimmedUserName -ne $null) {
+        $UserNameHasVariable = $trimmedUserName | ?<ARM_Variable> -Extract
 
-    if ($UserNameHasVariable) {
-        $variableValue = $TemplateObject.variables.($UserNameHasVariable.VariableName)
-        $variableValueExpression = $variableValue | ?<ARM_Template_Expression>
-        if (-not $variableValueExpression) {
-            Write-Error @"
+        if ($UserNameHasVariable) {
+            $variableValue = $TemplateObject.variables.($UserNameHasVariable.VariableName)
+            $variableValueExpression = $variableValue | ?<ARM_Template_Expression>
+            if (-not $variableValueExpression) {
+                Write-Error @"
 AdminUsername references variable '$($UserNameHasVariable.variableName)', which has a literal value.
 "@ -ErrorId AdminUserName.Is.Variable.Literal # write an error
+            }
         }
     }
 }
