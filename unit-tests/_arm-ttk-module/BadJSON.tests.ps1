0666e5d73a5fe9dd0d711f1b14878e01817da0f0
James Brundage <james.brundage@start-automating.com>
2021-06-12 22:37:49 -0700

    Adding tests for Import-JSON's handling of bad JSON

---
+++ unit-tests/_arm-ttk-module/BadJSON.tests.ps1
@@ -0,0 +1,13 @@
+﻿
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Push-Location -Path "$here"
+
+describe Import-JSON {
+    it 'Will report the name of the file along with any issues' {
+        Import-Json -FilePath .\bad.json -ErrorAction SilentlyContinue -ErrorVariable err        
+        "$err" | should -BeLike '*bad.json*'
+    }
+}
+
+Pop-Location
