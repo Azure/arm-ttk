f9b7c4bd1f7e832226f8ec83ebff5892a1065762
James Brundage <james.brundage@start-automating.com>
2021-06-15 15:18:09 -0700

    Adding AllFiles\JSONFiles-Should-Be-Valid

---
+++ arm-ttk/testcases/AllFiles/JSONFiles-Should-Be-Valid.test.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+    Ensures that all JSON files are valid
+.Description
+    Ensures that all JSON files are valid syntax and can be imported.
+#>
+param(
+[PSObject[]]
+$FolderFiles
+)
+
+foreach ($file in $FolderFiles) {
+    if ($file.FullPath -notmatch '\.json(c)?$') { continue }
+    $imported = Import-Json -FilePath $TemplateFullPath -ErrorAction SilentlyContinue -ErrorVariable ConvertIssue
+    if (-not $imported) {
+        Write-Error "Could not import '$TemplateFullPath'" -TargetObject $TemplateFullPath -ErrorId "Invalid.JSON"
+    }
+}
