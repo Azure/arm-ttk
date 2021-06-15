2c01d32d0cb10b2e3e5be3a7a6e2d6cd7dfa9369
James Brundage <james.brundage@start-automating.com>
2021-06-15 12:01:07 -0700

    Updating BadJSON.tests.ps1

--- unit-tests/_arm-ttk-module/BadJSON.tests.ps1
+++ unit-tests/_arm-ttk-module/BadJSON.tests.ps1
@@ -5,7 +5,7 @@ Push-Location -Path "$here"
 
 describe Import-JSON {
     it 'Will report the name of the file along with any issues' {
-        Import-Json -FilePath .\Bad.json -ErrorAction SilentlyContinue -ErrorVariable err        
+        Import-Json -FilePath .\Bad.json -ErrorAction SilentlyContinue -ErrorVariable err
         "$err" | should -BeLike '*Bad.json*'
     }
 }
