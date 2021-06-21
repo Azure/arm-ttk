f6e480d3946ee84892109558547bbf7a5ac80533
James Brundage <james.brundage@start-automating.com>
2021-06-21 11:10:43 -0700

    Fixing expansion test (wildcard incomplete)

--- unit-tests/_arm-ttk-module/ExpandVariables.tests.ps1
+++ unit-tests/_arm-ttk-module/ExpandVariables.tests.ps1
@@ -18,7 +18,7 @@ describe Expand-AzTemplate {
     context 'ExpandedVariables' {
         $expanded = Expand-AzTemplate -TemplatePath (Join-Path $pwd .\ExpandVariables.json)
         $expanded.ExpandedTemplateText | ?<ARM_Variable> | Should -Be $null
-        $expanded.ExpandedTemplateObject.someProperty | Should -BeLike 'resourceGroup()*.location*'
+        $expanded.ExpandedTemplateObject.someProperty | Should -BeLike '*resourceGroup()*.location*'
     }
 }
 
