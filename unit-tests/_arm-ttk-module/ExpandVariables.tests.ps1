94ac872f3b0521d2e7c0706e415cebc61b34af76
James Brundage <james.brundage@start-automating.com>
2021-06-21 10:56:15 -0700

    Adding tests for ExpandedVariables

---
+++ unit-tests/_arm-ttk-module/ExpandVariables.tests.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.Synopsis
+    Tests related to expansion
+.Description
+    Tests related to expanding an Azure Resource Manager Template.
+
+    Expand-AzTemplate expands a resource manager template into a series of well-known variables
+#>
+param(
+)
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+
+Push-Location -Path "$here"
+
+describe Expand-AzTemplate {
+    context 'ExpandedVariables' {
+        $expanded = Expand-AzTemplate -TemplatePath (Join-Path $pwd .\ExpandVariables.json)
+        $expanded.ExpandedTemplateText | ?<ARM_Variable> | Should -Be $null
+        $expanded.ExpandedTemplateObject.someProperty | Should -BeLike 'resourceGroup()*.location*'
+    }
+}
+
+Pop-Location
