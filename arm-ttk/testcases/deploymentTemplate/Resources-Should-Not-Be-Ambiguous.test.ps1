4eda5a1062f37efc34515a41d482655546669760
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2021-07-26 18:44:23 -0500

    add exception for proxy resources, fix error msg

--- arm-ttk/testcases/deploymentTemplate/Resources-Should-Not-Be-Ambiguous.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Resources-Should-Not-Be-Ambiguous.test.ps1
@@ -13,48 +13,60 @@
     * The related Resource contains some but not all of the name segments.
 #>
 param(
-# The object representation of an Azure Resource Manager template.
-[Parameter(Mandatory,Position=0)]
-$TemplateObject,
+    # The object representation of an Azure Resource Manager template.
+    [Parameter(Mandatory, Position = 0)]
+    $TemplateObject,
 
-# The text of an Azure Resource Manager template
-[Parameter(Mandatory,Position=1)]
-$TemplateText,
+    # The text of an Azure Resource Manager template
+    [Parameter(Mandatory, Position = 1)]
+    $TemplateText,
 
-[Parameter(Mandatory,Position=1)]
-$TemplateFullPath
+    [Parameter(Mandatory, Position = 1)]
+    $TemplateFullPath
 )
 
 # Find all uses of the function 'ResourceID'
+
+# certain resources are visible at any scope so should be excluded from the test
+$proxyResourceTypes = @(
+    'Microsoft.Authorization/roleDefinitions',
+    'Microsoft.Authorization/policyDefinitions',
+    'Microsoft.Authorization/policySetDefinitions'
+)
+
 $resourceIdFunctions = $TemplateText | ?<ARM_Template_Function> -FunctionName resourceId
 
 :nextResourceId foreach ($rid in $resourceIdFunctions) {
     $resourceIdParameters = @($rid.Groups["Parameters"] -split ',')
 
     $foundResourceType = ''
-    $resourceTypeIndex = -1
-    $foundApiVersion = ''
-    $additionalParameters = @(for ($n = 0 ;$n -lt $resourceIdParameters.Count;$n++) {
-        if ($resourceIdParameters[$n] -like '*/*') {
-            $foundResourceType = $resourceIdParameters[$n].Replace('(','').Replace("'","") # Remove extraneous chars before looking for the resource
-            if ($n -eq 0) { # If the resource type is the first parameter
-                $foundResource = # see if we can find a resource with that type.
-                    @(Find-JsonContent -InputObject $TemplateObject.resources -Key Type -Value $foundResourceType)
-                if ((-not $foundResource) -or ($foundResource | Where-Object Condition)) 
-                { 
-                    Write-Error "At least one parameter must preceed the resource type" -TargetObject $rid -ErrorId 'ResourceID.Missing.Name'
-                    continue nextResourceId
+    #    $resourceTypeIndex = -1
+    #    $foundApiVersion = ''
+    $additionalParameters = @(for ($n = 0 ; $n -lt $resourceIdParameters.Count; $n++) {
+            if ($resourceIdParameters[$n] -like '*/*') {
+                $foundResourceType = $resourceIdParameters[$n].Replace('(', '').Replace("'", "") # Remove extraneous chars before looking for the resource
+
+                # exclude certain types from the resourceGroupName check
+                if ($proxyResourceTypes -notcontains $foundResourceType) {
+                    if ($n -eq 0) {
+                        # If the resource type is the first parameter
+                        $foundResource = # see if we can find a resource with that type.
+                        @(Find-JsonContent -InputObject $TemplateObject.resources -Key Type -Value $foundResourceType)
+                        if ((-not $foundResource) -or ($foundResource | Where-Object Condition)) { 
+                            Write-Error "At least one parameter must preceed the resource type in: $rid" -TargetObject $rid -ErrorId 'ResourceID.Missing.Name'
+                            continue nextResourceId
+                        }
+                    }
+                    #$resourceTypeIndex = $n
                 }
             }
-            $resourceTypeIndex = $n
-        }
-        elseif ($n) {
-            $resourceIdParameters[$n] -replace 
-                            "^\s{0,}'{0,1}"  -replace 
-                            "\s{0,}'{0,1}$"  -replace 
-                            '\){0,}$'
-        }                       
-    })
+            elseif ($n) {
+                $resourceIdParameters[$n] -replace 
+                "^\s{0,}'{0,1}" -replace 
+                "\s{0,}'{0,1}$" -replace 
+                '\){0,}$'
+            }                       
+        })
 
     if ($foundResourceType) {
         $resourceTypeName = $foundResourceType -replace "^\s{0,}'" -replace "\s{0,}'$"        
@@ -65,23 +77,25 @@ $resourceIdFunctions = $TemplateText | ?<ARM_Template_Function> -FunctionName re
 
         # Find all resources of this type within the template
         $resourcesOfType = 
-            @(Find-JsonContent -InputObject $TemplateObject.resources -Key type -Value $resourceTypeName)            
+        @(Find-JsonContent -InputObject $TemplateObject.resources -Key type -Value $resourceTypeName)            
             
         
         # walk thru each resource of the type
         foreach ($resource in $resourcesOfType) {
             
             $foundParametersInResource = # See if we can find the additional parameters
-                @(foreach ($additionalParameter in $additionalParameters) {
+            @(foreach ($additionalParameter in $additionalParameters) {
                     $resource.name -like "*$additionalParameter*"
                 })
 
-            if ($foundParametersInResource.Count -gt 1) { # If we found any additional parameters
+            if ($foundParametersInResource.Count -gt 1) {
+                # If we found any additional parameters
                 # See if we have enough
                 if ($foundParametersInResource.Count -lt $additionalParameters.Count) {
                     # If we didn't have enough, we may want to write an error.
                     Write-Error "Resource referencing $rid does not contain all segments of it's resource name" -TargetObject $rid -ErrorId 'ResourceID.Missing.Name'
-                } else {
+                }
+                else {
                     # but if we did, we can finally feel ok about this resourceID.
                     continue nextResourceID
                 }
@@ -89,7 +103,7 @@ $resourceIdFunctions = $TemplateText | ?<ARM_Template_Function> -FunctionName re
         }
 
         $resourcesOfSubType = 
-            @(Find-JsonContent -InputObject $TemplateObject.resources -Key type -Value @($resourceTypeName -split '/' -ne '')[-1])
+        @(Find-JsonContent -InputObject $TemplateObject.resources -Key type -Value @($resourceTypeName -split '/' -ne '')[-1])
 
         # walk thru each resource of the type
         foreach ($resource in $resourcesOfSubType) {
@@ -100,16 +114,18 @@ $resourceIdFunctions = $TemplateText | ?<ARM_Template_Function> -FunctionName re
                 continue
             }
             $foundParametersInResource = # See if we can find the additional parameters
-                @(foreach ($additionalParameter in $additionalParameters) {
+            @(foreach ($additionalParameter in $additionalParameters) {
                     $resource.name -like "*$additionalParameter*"
                 })
 
-            if ($foundParametersInResource.Count -gt 1) { # If we found any additional parameters
+            if ($foundParametersInResource.Count -gt 1) {
+                # If we found any additional parameters
                 # See if we have enough
                 if ($foundParametersInResource.Count -lt $additionalParameters.Count) {
                     # If we didn't have enough, we may want to write an error.
                     Write-Error "Resource referencing $rid does not contain all segments of it's resource name" -TargetObject $rid -ErrorId 'ResourceID.Missing.Name'
-                } else {
+                }
+                else {
                     # but if we did, we can finally feel ok about this resourceID.
                     continue nextResourceID
                 }
