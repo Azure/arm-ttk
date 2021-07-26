32b44a74ce8e4eb2d55e5e97f78e721a12597157
James Brundage <james.brundage@start-automating.com>
2021-07-26 13:32:10 -0700

    Improving Support for Copy Variables (Fixes #338)

--- arm-ttk/testcases/deploymentTemplate/Variables-Must-Be-Referenced.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Variables-Must-Be-Referenced.test.ps1
@@ -10,38 +10,9 @@ param(
     $TemplateObject,
 
     [Parameter(Mandatory = $true, Position = 1)]
-    [PSObject]
+    [string]
     $TemplateText
 )
-
-$findVariableInTemplate = {
-    # Create a Regex to find the variable
-    param(
-        [Parameter(Mandatory,Position=0,ValueFromPipeline)]
-        [string]$Name
-    )
-    
-    process {
-        
-        $escapedName = $name -replace '\s', '\s'
-        
-        [Regex]::new(@"
-            variables    # the variables keyword
-            \s{0,}       # optional whitespace
-            \(           # opening parenthesis
-            \s{0,}       # more optional whitespace
-            '            # a single quote
-            $escapedName # the variable name
-            '            # either a single quote
-            \s{0,}       # more optional whitespace
-            \)           # closing parenthesis
-"@,
-        # The Regex needs to be case-insensitive
-        'Multiline,IgnoreCase,IgnorePatternWhitespace'
-        ).Matches($TemplateText) | 
-            Add-Member NoteProperty Name $Name -Force -PassThru
-    }
-}
 
 $exprStrOrQuote = [Regex]::new('(?<!\\)[\[\"]', 'RightToLeft')
 foreach ($variable in $TemplateObject.variables.psobject.properties) {
@@ -49,7 +20,7 @@ foreach ($variable in $TemplateObject.variables.psobject.properties) {
     # TODO: if the variable name is "copy": we need to loop through the array and pull each var and check individually
     
     if ($variable.name -ne 'copy' -and $variable.value.copy -eq $null) {        
-        $foundRefs = @(& $findVariableInTemplate $variable.Name)
+        $foundRefs = $TemplateText | ?<ARM_Variable> -Variable $variable.Name        
         if (-not $foundRefs) {
             Write-Error -Message "Unreferenced variable: $($Variable.Name)" -ErrorId Variables.Must.Be.Referenced -TargetObject $variable
         } else {
@@ -68,9 +39,20 @@ foreach ($variable in $TemplateObject.variables.psobject.properties) {
                 $variable.value.copy
             }
         foreach ($copyItem in $copyItemList) {           
-            $foundRefs = @(& $findVariableInTemplate $copyItem.Name)
+            $foundRefs = $TemplateText | ?<ARM_Variable> -Variable $copyItem.Name
             if (-not $foundRefs) {
-                Write-Error -Message "Unreferenced variable: $($copyItem.Name)" -ErrorId Variables.Must.Be.Referenced -TargetObject $copyItem
+                # if we have not found a reference for that variable, see if we have a dotted property of this name within the main variable reference.
+                $foundMainVariable = $TemplateText | ?<ARM_Variable> -Variable $variable.Name
+                $isOk = $true
+                if (-not $foundMainVariable) { 
+                    $isOk = $false
+                }
+                if ($TemplateText.Substring($foundMainVariable.EndIndex + 1, $copyItem.Name.Length) -ne $copyItem.Name) {
+                    $isOk = $false
+                }
+                if (-not $isOk) {
+                    Write-Error -Message "Unreferenced variable: $($copyItem.Name)" -ErrorId Variables.Must.Be.Referenced -TargetObject $copyItem
+                }
             } else {
                 foreach ($fr in $foundRefs) {
                     $foundQuote = $exprStrOrQuote.Match($TemplateText, $fr.Index)
