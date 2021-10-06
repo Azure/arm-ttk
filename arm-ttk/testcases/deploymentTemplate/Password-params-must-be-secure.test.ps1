b5e1e9306a44c6b8242251270f63a3359fdc0ddf
Brian Moore (AZURE RESOURCE MANAGER) <bmoore@microsoft.com>
2021-10-06 12:55:06 -0500

    added more param names and some exceptions

--- arm-ttk/testcases/deploymentTemplate/Password-params-must-be-secure.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/Password-params-must-be-secure.test.ps1
@@ -9,9 +9,9 @@
     .\Password-params-must-be-secure.test.ps1 -TemplateObject (Get-Content ..\..\..\unit-tests\Password-params-must-be-secure.test.json -Raw | ConvertFrom-Json)
 #>
 param(
-[Parameter(Mandatory=$true,Position=0)]
-[PSObject]
-$TemplateObject
+    [Parameter(Mandatory = $true, Position = 0)]
+    [PSObject]
+    $TemplateObject
 )
 
 <#
@@ -33,11 +33,31 @@ foreach ($parameter in $templateObject.parameters.psobject.properties) {
     $name = $parameter.name
     
     # using a name matching pattern to decide if this should be secured or not
-    if($name -like "*password*"){
+    if ($name -like "*password*" -or 
+        $name -like "*secret*" -or
+        $name -like "*accountkey*") {
         # if it's not secure, flag it
+
         if ($type -ne 'securestring' -and $type -ne 'secureobject') {
-            Write-Error -Message "Parameter `"$name`" is of type `"$type`" but should be secure." -ErrorId Password.Param.Not.Secure -TargetObject $parameter
-        }      
+            #except certain patterns we know about in ARM
+            # secret + Permissions (keyVault secret perms is an accessPolicy property)
+            # secret + Version (url or simply the version property of a secret)
+            # secret + url
+            # secret + name
+            if ($name -like "*secret*" -and
+                   ($name -like "*permission*" -or
+                    $name -like "*version*" -or
+                    $name -like "*url*" -or
+                    $name -like "*uri*" -or 
+                    $name -like "*name*")
+                )
+            {
+                Write-Warning "Skipping parameter `"$name`""
+            }
+            else {
+                Write-Error -Message "Parameter `"$name`" is of type `"$type`" but should be secure." -ErrorId Password.Param.Not.Secure -TargetObject $parameter
+            }      
+        }
     }
 }
 
