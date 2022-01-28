07312901570f56ad080d19aa6d11be026e576145
James Brundage <james.brundage@start-automating.com>
2022-01-28 13:29:40 -0800

    Fixing #418 : Checking VMSize property in any resource (#578)
    * Fixing #418 : Checking VMSize property in any resource
    
    * Update VMSize-In-Alternate-VMTypes.json
    
    * Adding positive case
    
    * Update VMSize-In-Alternate-VMTypes.json
    
    * Update VMSize-In-Alternate-VMTypes.json
    
    Co-authored-by: Brian Moore <bmoore@microsoft.com>

--- arm-ttk/testcases/deploymentTemplate/VM-Size-Should-Be-A-Parameter.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/VM-Size-Should-Be-A-Parameter.test.ps1
@@ -10,28 +10,11 @@ param(
 $TemplateObject
 )
 
-$vms = $TemplateObject.resources | Find-JsonContent -Key type -Value Microsoft.Compute/virtualMachines
+$vmSizes = $TemplateObject.resources | Find-JsonContent -Key vmSize -Value * -Like
 
-
-foreach ($vm in $vms) {
-    if ($vm.plan) {
-        Write-Verbose "Skipping $($vm.Name) because it has a plan"
-        continue
-    }
-    $hardwareProfile = $vm.properties.hardwareProfile
-
-    if ($hardwareProfile -is [string]) { # If the hardwareProfile was a string,
-        # set hardwareProfile to the resolved expression
-        $hardwareProfile = Expand-AzTemplate -Expression $hardwareProfile -InputObject $TemplateObject
-    }
-
-    if (-not $hardwareProfile) { # If the hardwareProfile didn't resolve
-        Write-Error "Could not resolve hardware profile" -TargetObject $vm # write an error
-        continue # and move onto the next
-    }
-
-
-    $vmSize = $hardwareProfile.vmSize
+foreach ($vmSizeObject in $vmSizes) {
+    
+    $vmSize = $vmSizeObject.vmSize
 
     if ($vmSize -notmatch "\s{0,}\[.*?parameters\s{0,}\(\s{0,}'") {
         if ($vmSize -match "\s{0,}\[.*?variables\s{0,}\(\s{0,}'") {
