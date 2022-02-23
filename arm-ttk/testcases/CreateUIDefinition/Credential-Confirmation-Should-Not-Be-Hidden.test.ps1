200087d234b891c54b60bc98533307ac8068e37f
v-rucdu <v-rucdu@microsoft.com>
2022-02-23 11:55:18 +0530

    Enhancing the passwordbox validation

--- arm-ttk/testcases/CreateUIDefinition/Credential-Confirmation-Should-Not-Be-Hidden.test.ps1
+++ arm-ttk/testcases/CreateUIDefinition/Credential-Confirmation-Should-Not-Be-Hidden.test.ps1
@@ -14,22 +14,22 @@ $CreateUIDefinitionObject
 )
 
 # First, find all password boxes.
-$passwordBoxes = $CreateUIDefinitionObject | 
+$passwordBoxes = $CreateUIDefinitionObject |
     Find-JsonContent -Key type -Value Microsoft.Common.PasswordBox
- 
+
 # Then find all CredentialsCombos.
-$credentialComboBoxes = $CreateUIDefinitionObject | 
+$credentialComboBoxes = $CreateUIDefinitionObject |
     Find-JsonContent -Key type -Value Microsoft.Compute.CredentialsCombo
 
 # Put them together into one list.
 $allCredentialBoxes =  @() + $passwordBoxes + $credentialComboBoxes
 
 
-foreach ($credBox in $allCredentialBoxes) { # Walk thru the list 
-   
-    if ($credBox.options.hideConfirmation -eq $true) { # If the options has hideConfirmation set to true
-        # write an error
-        Write-Error "`"hideConfirmation`" must not be true for credentials" -TargetObject $credBox -ErrorId Confirmation.Should.Not.Be.Hidden
-    }
+foreach ($credBox in $allCredentialBoxes) { # Walk thru the list
+   if (($credBox.options.hideConfirmation -eq $true) -and !($credBox.name.ToLower() -match (@("key","apikey") -join '|')))
+   {
+       # If the options has hideConfirmation set to true
+       # write an error
+       Write-Error "`"hideConfirmation`" must not be true for credentials" -TargetObject $credBox -ErrorId Confirmation.Should.Not.Be.Hidden
+   }
 }
- 
