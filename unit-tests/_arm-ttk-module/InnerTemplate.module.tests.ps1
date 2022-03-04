c944cb45fdb5c942aa0a745c6c166b9f5b1e9b02
James Brundage <@github.com>
2022-03-04 11:12:17 -0800

    Adding detailed test for innertemplates

--- unit-tests/_arm-ttk-module/InnerTemplate.module.tests.ps1
+++ unit-tests/_arm-ttk-module/InnerTemplate.module.tests.ps1
@@ -14,13 +14,14 @@ Push-Location -Path "$here\..\..\arm-ttk"
 
 describe InnerTemplates {
     it 'Will only generate a failure from an InnerTemplate' {
-        $here | 
+        $testOutput = $here | 
             Split-Path | 
             Join-Path -ChildPath DeploymentTemplate-Must-Not-Contain-Hardcoded-Uri | 
             Join-Path -ChildPath Fail | 
             Join-Path -ChildPath Hardcoded-Uri-In-InnerTemplate.json |
             Get-ChildItem |
-            Test-AzTemplate -Test DeploymentTemplate-Must-Not-Contain-Hardcoded-Uri |
+            Test-AzTemplate -Test DeploymentTemplate-Must-Not-Contain-Hardcoded-Uri
+        $testOutput |
             Measure-Object |
             Select-Object -ExpandProperty Count |
             Should -Be 1
@@ -36,5 +37,33 @@ describe InnerTemplates {
         $templatePath = $here | Join-Path -ChildPath InnerTemplateWithEscapeSequence.json
         $expanded = Expand-AzTemplate -TemplatePath $templatePath
         $expanded.innerTemplates.Count | Should -be 1
+    }   $testOutput   = 
+    $testOutput   = 
+    it 'Will expand inner templates and their grandchildren correctly' {
+        $templatePath = $here | Join-Path -ChildPath MultipleInnerTemplatesWithUnreferencedParameters.json
+        $testOutput   = Test-AzTemplate -TemplatePath $templatePath -Test "Parameters Must Be Referenced"
+        # There should be 5 outputs
+        $testOutput.Count | Should -be 5
+        # The first should come from root
+        $testOutput[0].AllOutput[0].TargetObject.JSONPath | Should -Be parameters.NotUsedAtRoot
+        
+        # The next template should be "NotUserInInnerTemplate1" (the first inner template)
+        $testOutput[1].AllOutput[0].TargetObject.JSONPath | Should -Be parameters.NotUsedInInnerTemplate1
+
+        # The location of the inner template errors should be adjusted, thus the .Line should be greater
+        $testOutput[1].AllOutput[0].Location.Line | Should -BeGreaterThan $testOutput[0].AllOutput[0].Location.Line
+
+        # The next template should be "NotUsedInInnerInnerTemplate1" (the first grandchild)
+        $testOutput[2].AllOutput[0].TargetObject.JSONPath | Should -Be parameters.NotUsedInInnerInnerTemplate1
+        $testOutput[2].AllOutput[0].Location.Line | Should -BeGreaterThan $testOutput[1].AllOutput[0].Location.Line
+    
+        
+        # The next template should be "NotUsedInInnerTemplate2" (the second template)
+        $testOutput[3].AllOutput[0].TargetObject.JSONPath | Should -Be parameters.NotUsedInInnerTemplate2
+        $testOutput[3].AllOutput[0].Location.Line | Should -BeGreaterThan $testOutput[2].AllOutput[0].Location.Line
+
+        # The next template should be "NotUsedInInnerInnerTemplate2" (the second grandchild)
+        $testOutput[4].AllOutput[0].TargetObject.JSONPath | Should -Be parameters.NotUsedInInnerInnerTemplate2
+        $testOutput[4].AllOutput[0].Location.Line | Should -BeGreaterThan $testOutput[3].AllOutput[0].Location.Line
     }
 }
