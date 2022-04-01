4bd4781f4f59b2bda23c15a551b0a919d606f833
Brian Moore <bmoore@microsoft.com>
2022-04-01 17:25:32 -0500

    improved error message (#628)

--- arm-ttk/testcases/deploymentTemplate/VM-Size-Should-Be-A-Parameter.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/VM-Size-Should-Be-A-Parameter.test.ps1
@@ -5,9 +5,9 @@
     Ensures that the Sizes of a virtual machine in a template are parameters
 #>
 param(
-[Parameter(Mandatory=$true)]
-[PSObject]
-$TemplateObject
+    [Parameter(Mandatory = $true)]
+    [PSObject]
+    $TemplateObject
 )
 
 $vmSizes = $TemplateObject.resources | Find-JsonContent -Key vmSize -Value * -Like
@@ -15,15 +15,19 @@ $vmSizes = $TemplateObject.resources | Find-JsonContent -Key vmSize -Value * -Li
 foreach ($vmSizeObject in $vmSizes) {
     
     $vmSize = $vmSizeObject.vmSize
+    $resourceType = $vmSizeObject.ParentObject.type
+    $resourceName = $vmSizeObject.ParentObject.name
 
     if ($vmSize -notmatch "\s{0,}\[.*?parameters\s{0,}\(\s{0,}'") {
         if ($vmSize -match "\s{0,}\[.*?variables\s{0,}\(\s{0,}'") {
             $resolvedVmSize = Expand-AzTemplate -Expression $vmSize -InputObject $TemplateObject
             if ($resolvedVmSize -notmatch "\s{0,}\[.*?parameters\s{0,}\(\s{0,}'") {
-                Write-Error "VM Size must be a parameter" -TargetObject $vm
+                Write-Error "VM Size for resourceType '$resourceType' named '$resourceName' must be a parameter" -TargetObject $vm
             }
-        } else {
-            Write-Error "VM Size must be a parameter" -TargetObject $vm
+        }
+        else {
+            Write-Error "VM Size for resourceType '$resourceType' named '$resourceName' must be a parameter" -TargetObject $vm
         }
     }
+
 }
