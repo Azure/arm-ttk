9465ec7090784cc5512ad5b0516c3237cee1ab9f
Brian Moore <bmoore@microsoft.com>
2022-04-08 14:54:54 -0500

    Update Test-AzTemplate.cmd

--- arm-ttk/Test-AzTemplate.cmd
+++ arm-ttk/Test-AzTemplate.cmd
@@ -1,1 +1,1 @@
-powershell.exe -noprofile -nologo -command "Import-Module '%~dp0arm-ttk.psd1'; Test-AzTemplate %*; if ($Errors.Count) { exit 1}"
+powershell.exe -noprofile -nologo -command "Import-Module '%~dp0arm-ttk.psd1'; Test-AzTemplate %*; if ($Error.Count) { exit 1}"
