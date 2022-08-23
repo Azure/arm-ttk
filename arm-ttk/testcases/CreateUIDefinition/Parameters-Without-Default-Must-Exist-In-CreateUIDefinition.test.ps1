802831eb3addd400a98a95e5f945c7ec068eb2fa
Ross Cousens <rcousens@users.noreply.github.com>
2022-08-23 14:59:25 +1000

    Fix typo in CreateUIDefinition defaultValues testcase

--- arm-ttk/testcases/CreateUIDefinition/Parameters-Without-Default-Must-Exist-In-CreateUIDefinition.test.ps1
+++ arm-ttk/testcases/CreateUIDefinition/Parameters-Without-Default-Must-Exist-In-CreateUIDefinition.test.ps1
@@ -1,8 +1,8 @@
 ﻿<#
 .Synopsis
-    Ensure that parameters that don't have defaultValues are defined CreateUIDefinition.outuputs
+    Ensure that parameters that don't have defaultValues are defined CreateUIDefinition.outputs
 .Description
-    Ensure that parameters that don't have defaultValues are defined CreateUIDefinition.outuputs
+    Ensure that parameters that don't have defaultValues are defined CreateUIDefinition.outputs
 #>
 
 param(
@@ -21,7 +21,7 @@ foreach ($parameter in $TemplateObject.parameters.psobject.properties) {
     $defaultValue = $parameterInfo.defaultValue
     if ($defaultValue -eq $null) { # empty string is ok, only missing defaultValues should be flagged
         if ($CreateUIDefinitionObject.parameters.outputs.$parameterName -eq $null) {
-            Write-Error "$parameterName does not have a default value, and is not defined in CreateUIDefinition.outuputs" -ErrorId Parameter.Without.Default.Missing.From.CreateUIDefinition -TargetObject $TemplateObject.parameters
+            Write-Error "$parameterName does not have a default value, and is not defined in CreateUIDefinition.outputs" -ErrorId Parameter.Without.Default.Missing.From.CreateUIDefinition -TargetObject $TemplateObject.parameters
             continue
         }
     }
