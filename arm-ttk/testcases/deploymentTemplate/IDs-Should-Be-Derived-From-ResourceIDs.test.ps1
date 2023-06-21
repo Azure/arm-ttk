d0c93b093a2a1c6aaa0275cfd079a8ed0a7f14a5
v-sabiraj <v-sabiraj@microsoft.com>
2023-06-21 14:54:23 +0530

    Update IDs-Should-Be-Derived-From-ResourceIDs.test.ps1

--- arm-ttk/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
@@ -89,8 +89,8 @@ foreach ($id in $ids) {
         "policyDefinitionGuid"              # Microsoft.Sentinel/Solutions/AzureStorageAccount
         "IndicatorId"                       # Network Session Solution for Microsoft
         "aws_iam_functionapp_id"            # Microsoft.Sentinel/Solutions/AWS_IAM
-        "contentProductId"
-        "id"
+        "contentProductId"                  # Microsoft.Sentinel/Solutions
+        "id"                                # Microsoft.Sentinel/Solutions
     )
 
     $exceptionRegex =
