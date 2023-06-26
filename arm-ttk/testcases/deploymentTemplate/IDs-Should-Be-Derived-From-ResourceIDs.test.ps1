f0f4d16c3640288ee46215a2e749884fa305c5ae
v-sabiraj <v-sabiraj@microsoft.com>
2023-06-26 15:41:08 +0530

    Update IDs-Should-Be-Derived-From-ResourceIDs.test.ps1

--- arm-ttk/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/IDs-Should-Be-Derived-From-ResourceIDs.test.ps1
@@ -90,6 +90,7 @@ foreach ($id in $ids) {
         "IndicatorId",                       # Network Session Solution for Microsoft
         "aws_iam_functionapp_id",            # Microsoft.Sentinel/Solutions/AWS_IAM
         "contentProductId"                 # Microsoft.Sentinel/Solutions
+        "id"                               # Microsoft.Sentinel/Solutions
         )
 
     $exceptionRegex =
