31bfe8ac206ab2cb18a4f20a326127b8d2871777
Pallavi Sah <pallavisah@microsoft.com>
2023-10-27 16:19:07 -0700

    Fixing powershell version

--- arm-ttk/arm-ttk.psm1
+++ arm-ttk/arm-ttk.psm1
@@ -1,6 +1,6 @@
 ﻿#region JSON Functions
 #powershell 6+ provides support for json with comments
-if ($PSVersionTable.PSVersion.Major -lt 5) {
+if ($PSVersionTable.PSVersion.Major -lt 6) {
     . $psScriptRoot\ConvertFrom-Json.ps1 # Overwriting ConvertFrom-JSON to allow for comments within JSON (not on core)
 }
 
