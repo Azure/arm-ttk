23fe9f107193f397024fbdbfa51668a500f8dda1
Pallavi Sah <pallavisah@microsoft.com>
2025-03-20 13:34:30 -0700

    Addressing pr comments

--- arm-ttk/testcases/deploymentTemplate/adminPassword-Should-Not-Be-A-Literal.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/adminPassword-Should-Not-Be-A-Literal.test.ps1
@@ -14,63 +14,123 @@ param(
 # Filtering the complete $TemplateObject directly fails with "The script failed due to call depth overflow." errors
 
 if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
-    $adminPasswordRefsResources = $TemplateObject.resources |
+    $adminLoginPasswordRefsResources = $TemplateObject.resources |
     Find-JsonContent -Key administratorLoginPassword  -Value * -Like 
 
-    foreach ($ref in $adminPasswordRefsResources) {
+    foreach ($ref in $adminLoginPasswordRefsResources) {
         # Walk over each one
         # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
         $a = $ref.administratorLoginPassword
         if ($a -isnot [string]) { 
             #check to see if this is a param value on a nested deployment - it will have a value property
             if ($a.value -is [string]) {
-                $trimmedPassword = "$($a.value)".Trim()
+                $trimmedAdminLoginPassword = "$($a.value)".Trim()
             }
             else {
                 continue # since we don't know what object shape we're testing at this point (could be a param declaration on a nested deployment)
             }
         }
         else {
-            $trimmedPassword = "$($a)".Trim()
+            $trimmedAdminLoginPassword = "$($a)".Trim()
         }
-        if ($trimmedPassword -notmatch '\[[^\]]+\]') {
+        if ($trimmedAdminLoginPassword -notmatch '\[[^\]]+\]') {
             # If they aren't expressions
-            Write-Error -TargetObject $ref -Message "AdministratorLoginPassword `"$trimmedPassword`" is not an expression" -ErrorId AdminPassword.Is.Literal # write an error
+            Write-Error -TargetObject $ref -Message "AdministratorLoginPassword `"$trimmedAdminLoginPassword`" is not an expression" -ErrorId AdminPassword.Is.Literal # write an error
+            continue # and move onto the next
+        }
+    }
+
+    $adminPasswordRefsResources = $TemplateObject.resources |
+    Find-JsonContent -Key adminPassword  -Value * -Like 
+
+    foreach ($ref in $adminPasswordRefsResources) {
+        # Walk over each one
+        # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
+        $a = $ref.adminPassword
+        if ($a -isnot [string]) { 
+            #check to see if this is a param value on a nested deployment - it will have a value property
+            if ($a.value -is [string]) {
+                $trimmedAdminPassword = "$($a.value)".Trim()
+            }
+            else {
+                continue # since we don't know what object shape we're testing at this point (could be a param declaration on a nested deployment)
+            }
+        }
+        else {
+            $trimmedAdminPassword = "$($a)".Trim()
+        }
+        if ($trimmedAdminPassword -notmatch '\[[^\]]+\]') {
+            # If they aren't expressions
+            Write-Error -TargetObject $ref -Message "AdminPassword `"$trimmedAdminPassword`" is not an expression" -ErrorId AdminPassword.Is.Literal # write an error
             continue # and move onto the next
         }
     }
 }
 
 if ("variables" -in $TemplateObject.PSobject.Properties.Name) {
-    $adminPasswordRefsVariables = $TemplateObject.variables |
+    $adminLoginPasswordRefsVariables = $TemplateObject.variables |
     Find-JsonContent -Key administratorLoginPassword  -Value * -Like
 
-    foreach ($ref in $adminPasswordRefsVariables) {
+    foreach ($ref in $adminLoginPasswordRefsVariables) {
         # Walk over each one
         # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
         if ($ref.administratorLoginPassword -isnot [string]) { continue }
-        $trimmedPassword = "$($ref.administratorLoginPassword)".Trim()
-        if ($trimmedPassword -notmatch '\[[^\]]+\]') {
+        $trimmedAdminLoginPassword = "$($ref.administratorLoginPassword)".Trim()
+        if ($trimmedAdminLoginPassword -notmatch '\[[^\]]+\]') {
             # If they aren't expressions
-            Write-Error -TargetObject $ref -Message "AdminPassword `"$trimmedPassword`" is variable which is not an expression" -ErrorId AdminPassword.Var.Is.Literal # write an error
+            Write-Error -TargetObject $ref -Message "AdminLoginPassword `"$trimmedAdminLoginPassword`" is variable which is not an expression" -ErrorId AdminPassword.Var.Is.Literal # write an error
             continue # and move onto the next
         }
     }
 
     # TODO - irregular doesn't handle null gracefully so we need to test for it
-    if ($trimmedPassword -ne $null) {
-        $UserNameHasVariable = $trimmedPassword | ?<ARM_Variable> -Extract
+    if ($trimmedAdminLoginPassword -ne $null) {
+        $LoginPasswordHasVariable = $trimmedAdminLoginPassword | ?<ARM_Variable> -Extract
         # this will return the outer most function in the expression
-        $userNameHasFunction = $trimmedPassword | ?<ARM_Template_Function> -Extract
+        $LoginPasswordHasFunction = $trimmedAdminLoginPassword | ?<ARM_Template_Function> -Extract
 
         # If we had a variable reference (not inside of another function) - then check it
         # TODO this will not flag things like concat so we should add a blacklist here to ensure it's still not a static or deterministic username
-        if ($UserNameHasVariable -and $userNameHasFunction.FunctionName -eq 'variables') { 
-            $variableValue = $TemplateObject.variables.($UserNameHasVariable.VariableName)
+        if ($LoginPasswordHasVariable -and $LoginPasswordHasFunction.FunctionName -eq 'variables') { 
+            $variableValue = $TemplateObject.variables.($LoginPasswordHasVariable.VariableName)
             $variableValueExpression = $variableValue | ?<ARM_Template_Expression>
             if (-not $variableValueExpression) {
                 Write-Error @"
-AdminPassword references variable '$($UserNameHasVariable.variableName)', which has a literal value.
+AdminLoginPassword references variable '$($LoginPasswordHasVariable.variableName)', which has a literal value.
+"@ -ErrorId AdminPassword.Is.Variable.Literal # write an error
+            }
+        }
+    }
+
+    $adminPasswordRefsVariables = $TemplateObject.variables |
+    Find-JsonContent -Key adminPassword  -Value * -Like
+
+    foreach ($ref in $adminPasswordRefsVariables) {
+        # Walk over each one
+        # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
+        if ($ref.adminPassword -isnot [string]) { continue }
+        $trimmedAdminPassword = "$($ref.adminPassword)".Trim()
+        if ($trimmedAdminPassword -notmatch '\[[^\]]+\]') {
+            # If they aren't expressions
+            Write-Error -TargetObject $ref -Message "AdminPassword `"$trimmedAdminPassword`" is variable which is not an expression" -ErrorId AdminPassword.Var.Is.Literal # write an error
+            continue # and move onto the next
+        }
+    }
+
+    # TODO - irregular doesn't handle null gracefully so we need to test for it
+    if ($trimmedAdminPassword -ne $null) {
+        $LoginPasswordHasVariable = $trimmedAdminPassword | ?<ARM_Variable> -Extract
+        # this will return the outer most function in the expression
+        $LoginPasswordHasFunction = $trimmedAdminPassword | ?<ARM_Template_Function> -Extract
+
+        # If we had a variable reference (not inside of another function) - then check it
+        # TODO this will not flag things like concat so we should add a blacklist here to ensure it's still not a static or deterministic username
+        if ($LoginPasswordHasVariable -and $LoginPasswordHasFunction.FunctionName -eq 'variables') { 
+            $variableValue = $TemplateObject.variables.($LoginPasswordHasVariable.VariableName)
+            $variableValueExpression = $variableValue | ?<ARM_Template_Expression>
+            if (-not $variableValueExpression) {
+                Write-Error @"
+AdminPassword references variable '$($LoginPasswordHasVariable.variableName)', which has a literal value.
 "@ -ErrorId AdminPassword.Is.Variable.Literal # write an error
             }
         }
--- arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
+++ arm-ttk/testcases/deploymentTemplate/adminUsername-Should-Not-Be-A-Literal.test.ps1
@@ -17,9 +17,6 @@ if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
     $adminUserNameRefsResources = $TemplateObject.resources |
     Find-JsonContent -Key adminUsername  -Value * -Like 
 
-    $adminLoginRefsResources = $TemplateObject.resources |
-    Find-JsonContent -Key administratorLogin  -Value * -Like
-
     foreach ($ref in $adminUserNameRefsResources) {
         # Walk over each one
         # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
@@ -43,6 +40,10 @@ if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
         }
     }
 
+    
+    $adminLoginRefsResources = $TemplateObject.resources |
+    Find-JsonContent -Key administratorLogin  -Value * -Like
+
     foreach ($ref in $adminLoginRefsResources) {
         # Walk over each one
         # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
@@ -50,18 +51,18 @@ if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
         if ($a -isnot [string]) { 
             #check to see if this is a param value on a nested deployment - it will have a value property
             if ($a.value -is [string]) {
-                $trimmedUserName = "$($a.value)".Trim()
+                $trimmedAdminLogin = "$($a.value)".Trim()
             }
             else {
                 continue # since we don't know what object shape we're testing at this point (could be a param declaration on a nested deployment)
             }
         }
         else {
-            $trimmedUserName = "$($a)".Trim()
+            $trimmedAdminLogin = "$($a)".Trim()
         }
-        if ($trimmedUserName -notmatch '\[[^\]]+\]') {
+        if ($trimmedAdminLogin -notmatch '\[[^\]]+\]') {
             # If they aren't expressions
-            Write-Error -TargetObject $ref -Message "AdministratorLogin `"$trimmedUserName`" is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
+            Write-Error -TargetObject $ref -Message "AdministratorLogin `"$trimmedAdminLogin`" is not an expression" -ErrorId AdminUsername.Is.Literal # write an error
             continue # and move onto the next
         }
     }
@@ -70,9 +71,6 @@ if ("resources" -in $TemplateObject.PSobject.Properties.Name) {
 if ("variables" -in $TemplateObject.PSobject.Properties.Name) {
     $adminUserNameRefsVariables = $TemplateObject.variables |
     Find-JsonContent -Key adminUsername  -Value * -Like
-
-    $administratorLoginRefsVariables = $TemplateObject.variables |
-    Find-JsonContent -Key administratorLogin  -Value * -Like
 
     foreach ($ref in $adminUserNameRefsVariables) {
         # Walk over each one
@@ -105,23 +103,27 @@ AdminUsername references variable '$($UserNameHasVariable.variableName)', which 
         }
     }
 
+    
+    $administratorLoginRefsVariables = $TemplateObject.variables |
+    Find-JsonContent -Key administratorLogin  -Value * -Like
+
     foreach ($ref in $administratorLoginRefsVariables) {
         # Walk over each one
         # if the property is not a string, then it's likely a param value for a nested deployment, and we should skip it.
         if ($ref.administratorLogin -isnot [string]) { continue }
-        $trimmedUserName = "$($ref.administratorLogin)".Trim()
-        if ($trimmedUserName -notmatch '\[[^\]]+\]') {
+        $trimmedAdminLogin = "$($ref.administratorLogin)".Trim()
+        if ($trimmedAdminLogin -notmatch '\[[^\]]+\]') {
             # If they aren't expressions
-            Write-Error -TargetObject $ref -Message "AdministratorLogin `"$trimmedUserName`" is variable which is not an expression" -ErrorId AdminUsername.Var.Is.Literal # write an error
+            Write-Error -TargetObject $ref -Message "AdministratorLogin `"$trimmedAdminLogin`" is variable which is not an expression" -ErrorId AdminUsername.Var.Is.Literal # write an error
             continue # and move onto the next
         }
     }
 
         # TODO - irregular doesn't handle null gracefully so we need to test for it
-    if ($trimmedUserName -ne $null) {
-        $UserNameHasVariable = $trimmedUserName | ?<ARM_Variable> -Extract
+    if ($trimmedAdminLogin -ne $null) {
+        $UserNameHasVariable = $trimmedAdminLogin | ?<ARM_Variable> -Extract
         # this will return the outer most function in the expression
-        $userNameHasFunction = $trimmedUserName | ?<ARM_Template_Function> -Extract
+        $userNameHasFunction = $trimmedAdminLogin | ?<ARM_Template_Function> -Extract
 
         # If we had a variable reference (not inside of another function) - then check it
         # TODO this will not flag things like concat so we should add a blacklist here to ensure it's still not a static or deterministic username
